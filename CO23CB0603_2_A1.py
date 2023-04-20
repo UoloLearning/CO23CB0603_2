c9344d2da0c3e3c13c298212e71dfd2be4ad340b
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-20 11:19:47 +0530

    Create CO23CB0603_2_A1.py

---
+++ CO23CB0603_2_A1.py
@@ -0,0 +1,10 @@
+ray_academics=65
+ray_analytical=8.5
+print (f"Ray's scores:\nAcademics: {ray_academics}%\nAnalytical test: {ray_analytical}")
+ray_condition = ray_academics>=60 and ray_analytical>=7
+print ("\nDoes Ray meet David's criteria?-",ray_condition)
+joe_academics=55
+joe_analytical=8
+print (f"\nJoe's scores:\nAcademics: {joe_academics}%\nAnalytical test: {joe_analytical}")
+joe_condition = joe_academics>=60 and joe_analytical>=77
+print ("\nDoes Ray meet David's criteria?-",joe_condition)
